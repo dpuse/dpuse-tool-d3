1c9a6047d4ac664f0fb79f7753c3b49267536ea2
Jonathan Terrell <terrell.jm@gmail.com>
2026-08-01 14:25:59 +0200

    v0.0.14

--- .scratch/named.js
+++
@@ -1,4 +0,0 @@
-import { barY, plot, ruleY } from '@observablehq/plot';
-export function run(rows, el) {
-  return plot({ marks: [barY(rows, { x: 'a', y: 'b' }), ruleY([0])] });
-}
--- .scratch/namespace.js
+++
@@ -1,4 +0,0 @@
-import * as Plot from '@observablehq/plot';
-export function run(rows, el) {
-  return Plot.plot({ marks: [Plot.barY(rows, { x: 'a', y: 'b' }), Plot.ruleY([0])] });
-}
